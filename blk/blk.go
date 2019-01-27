9463875b6098466b8f30005c0282d9950c756a7e
Shingo Kawamura <blp1526@gmail.com>
2019-01-28 01:10:06 +0900

    Add GetMajorMinor

--- blk/blk.go
+++ blk/blk.go
@@ -48,3 +48,39 @@ func GetPath(mountpoint string) (string, error) {
 
 	return "", ErrNotFound
 }
+
+// GetMajorMinor ...
+func GetMajorMinor(devPath string) (string, error) {
+	devName := filepath.Base(devPath)
+
+	fileInfos, err := ioutil.ReadDir(SysBlockPath)
+	if err != nil {
+		return "", err
+	}
+
+	path := ""
+	for _, fileInfo := range fileInfos {
+		fileInfoName := fileInfo.Name()
+		if strings.HasPrefix(devName, fileInfoName) {
+			path = filepath.Join(SysBlockPath, fileInfoName)
+			if devName != fileInfoName {
+				// name is a partition.
+				path = filepath.Join(path, devName)
+			}
+
+			path = filepath.Join(path, "dev")
+			break
+		}
+	}
+
+	if path == "" {
+		return "", ErrNotFound
+	}
+
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(b)), nil
+}
