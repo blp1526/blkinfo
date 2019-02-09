79aaec5cb5ce1e1a88860a25a1d5583de7c9a98a
Shingo Kawamura <blp1526@gmail.com>
2019-02-09 11:27:20 +0900

    Add go.mod

---
+++ tools.go
@@ -0,0 +1,8 @@
+//+build tools
+// via https://github.com/go-modules-by-example/index/blob/23a56e1095864bf596f2ce3aec296ecc89ab71b9/010_tools/README.md
+
+package main
+
+import (
+	_ "github.com/golangci/golangci-lint"
+)
