939efd824969c54aa7b059f0c9a3d5b41a2bd168
Shingo Kawamura <blp1526@gmail.com>
2019-02-10 19:11:29 +0900

    Add blkinfo.go

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/blp1526/go-udevinfo
+module github.com/blp1526/go-blkinfo
 
 require github.com/golangci/golangci-lint v1.13.2 // indirect
--- tools.go
+++ tools.go
@@ -1,7 +1,7 @@
 //+build tools
 // via https://github.com/go-modules-by-example/index/blob/23a56e1095864bf596f2ce3aec296ecc89ab71b9/010_tools/README.md
 
-package main
+package blkinfo
 
 import (
 	_ "github.com/golangci/golangci-lint"
